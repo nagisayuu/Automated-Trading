79562c33f947a7d2d0f6dd69c7a021796cdeb76d
Ubuntu <ubuntu@ip-172-31-16-123.ap-northeast-1.compute.internal>
2017-05-19 08:25:28 +0000

    プロトタイプ完成

--- src/controller/autoTradeMain.py
+++ src/controller/autoTradeMain.py
@@ -9,28 +9,20 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 def main():
 	# ユーザ情報、仮想通貨情報の入力
 	
-	# 売買の可否決定に必要な情報の取得
-	# →algorithmモジュールの中で実装することにしたので本箇所では記載不要
 	# 仮想通貨の購入または売却の可否を決定
 	import algorithm.thresholdSetting as alg
 	algexecuter=alg.AlgExecuter()
-	algexecuter.execute()
-	#VCInfo = getVCInfo()
-	#orderInfo = calcVCInfo(VCInfo)
+	result=algexecuter.execute()
 	# 仮想通貨の購入または売却
-	#k.query_private('AddOrder', {'pair': 'XXBTZEUR',
-        #                     'type': 'buy',
-        #                     'ordertype': 'limit',
-        #                     'price': '1',
-        #                     'volume': '1',
-        #                     'close[pair]': 'XXBTZEUR',
-        #                     'close[type]': 'sell',
-        #                     'close[ordertype]': 'limit',
-        #                     'close[price]': '9001',
-        #                     'close[volume]': '1'})
+	if result=="wait":
+		return 0
 
-#k = krakenex.API()
-#k.loadkey('kraken.key')
+	import krakenex
+	k = krakenex.API()
+	k.load_key('../../key/kraken.key')
+	response=k.query_private('AddOrder', {'pair': 'XXBTZEUR','type': 'buy','ordertype': 'market','volume': '1'})
+	print(response)
+
 
 if __name__ == "__main__":
 	main()
