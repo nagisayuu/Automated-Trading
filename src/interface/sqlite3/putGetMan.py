1251487017c227d7891d1c2c087009ffc102fd10
Ubuntu <ubuntu@ip-172-31-16-123.ap-northeast-1.compute.internal>
2017-05-20 08:58:53 +0000

    add:putGetMan.py

---
+++ src/interface/sqlite3/putGetMan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sqlite3
+
+class DBInstance:
+
+	def __init__(self):
+		self.dbname="autoTradeDB"
+
+table = tradelog
+
+	def connect(self,
+	
