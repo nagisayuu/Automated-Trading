17bd80b66c7236d5fe915524b843986686c54f11
trueno2315 <toueili@gmail.com>
2017-05-23 18:20:38 +0900

    update putGetMan.py

--- src/interface/sqlite3/putGetMan.py
+++ src/interface/sqlite3/putGetMan.py
@@ -5,11 +5,16 @@ import sqlite3
 
 class DBInstance:
 
-        def __init__(self):
-                conn = sqlite3.connect(tradelog.db)
-                conn.execute("create table tradelog(id,date,sprice,svolume,scur,bprice,bvilume,vcur)")
+#       conn.execute("create table tradelog(id,date,sprice,svolume,scur,bprice,bvilume,vcur)")
 
-        def get():
+        def get(self,STATEMENT):
+		conn = sqlite3.connect('tradelog.db')
+		cur = conn.cursor()
+		result = cur.execute(STATEMENT)
+		conn.close()
+		
+		return result
 
+#最新の情報を含む一行から
 
         def put():
